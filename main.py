32ccd06b2a090e92ba358bd2845285080b099ece
JiangChenrui <Jiangchenrui@bolegames.com>
2022-11-10 12:28:11 +0800

    22/11/10

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-# -*- coding: utf-8 -*-
-import math
 
-def get_club_activity_point(club_member_point_list):
-    cash_club_activity_point = sum([math.log((i/2000+1)) for i in club_member_point_list])
-    return round((10 * (0.2 - 1 / (10 - len(club_member_point_list) + 5)) * cash_club_activity_point), 1)
+from msilib.schema import MsiPatchHeaders
+import time
+import threading
+from concurrent.futures import ThreadPoolExecutor
+class Account(object):
+    """银行账户"""
+    def __init__(self):
+        self.balance = 0.0
+        self.lock = threading.Lock()
+    def deposit(self, money):
+        # 通过锁保护临界资源
+        with self.lock:
+            new_balance = self.balance + money
+            time.sleep(0.001)
+            self.balance = new_balance
 
-print get_club_activity_point([33333] * 2)
+
+def main():
+    """主函数"""
+    account = Account()
+    # 创建线程池
+    pool = ThreadPoolExecutor(max_workers=10)
+    futures = []
+    for _ in range(10000):
+        future = pool.submit(account.deposit, 1)
+        futures.append(future)
+    # 关闭线程池
+    pool.shutdown()
+    for future in futures:
+        future.result()
+    print(account.balance)
+
+
+if __name__ == '__main__':
+    main()
