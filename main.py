5b8d525081360b38678e9d844a87b63d71adb68c
JiangChenrui <1102592323@qq.com>
2024-04-11 17:11:53 +0800

    24/4/11

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
+import cv2
 
+# 读取图像
+img = cv2.imread('input.jpeg')
 
+# 应用高斯模糊
+blur_img = cv2.GaussianBlur(img, (155, 155), 0)
+
+# 保存处理后的图像
+cv2.imwrite('output.jpg', blur_img)
